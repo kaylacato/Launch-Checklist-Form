e5acaa7d4afd47e7b1d99a83c940d49ff72653ab
kaylacato <119690487+kaylacato@users.noreply.github.com>
2023-01-10 11:30:19 -0500

    Added validation for empty fields

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Write your JavaScript code here!
-
+window.addEventListener("load", function(){
+   let form = document.querySelector("#launchForm")
+   form.addEventListener("submit", function(event){
+      event.preventDefault();
+      let pilotInput = document.getElementById("pilotName");
+      let copilotInput = document.getElementById("copilotName");
+      let fuelInput = document.getElementById("fuelLevel");
+      let cargoInput = document.getElementById("cargoMass");
+      if (pilotInput.value === "" || copilotInput.value === "" || fuelInput.value === "" || cargoInput.value === "") {
+         alert("All fields are required!");
+      }
+      
+   })
+})
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ul>
@@ -11,3 +24,13 @@
 </ul>
 <img src="${}">
 */
+// TODO 1: set up a window load handler
+   // TODO 2: set up a submit handler for the form
+      // TODO 3: cancel submission using events.preventsDefault()
+// TODO 4: validate that all inputs have data in them
+// TODO 5: check fuelLevel and cargoMass and report launch status
+// TODO 6: make the list visible
+
+// TODO 7: fetch planet data
+   // TODO 8: randomly choose one of the planets
+   // TODO 9: display info of chosen planet
